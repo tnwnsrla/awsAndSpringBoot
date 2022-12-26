6b40ccd8ba54d6b8ec5948756eb5dbe71315d7d9
Jun_Water <tnwnsrla@naver.com>
2022-12-26 11:04:35 +0900

    SecurityConfig 스캔대상제거, 가짜인증사용자 추가

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,7 +23,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  *  단, @Service, @Component, @Repository  등은 사용할 수 없다.
  *  여기서는 컨트롤러만 사용하기 때문에 사용한다.
  */
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class
+            ,excludeFilters = {
+                //  SecurityConfig 는 읽었지만, SecurityConfig를 생성하기 위해 필요한 CustomOAtuh2UserService 는 읽을 수가 없어서 스캔 대상에서 SecurityConfig 제거
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+            }
+)
 public class HelloControllerTest {
 
     /**
@@ -29,6 +38,7 @@ public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -38,6 +48,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
